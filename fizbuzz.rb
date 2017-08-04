a72fcf6a19c7fe30f21100afe9c1865f96a59dc6
konradrog <konrad1210@gmail.com>
2017-08-04 10:44:58 +0200

    Done

---
+++ fizbuzz.rb
@@ -0,0 +1,16 @@
+def fizzbuzz
+  i = (1..100)
+  i.each do |el|
+    if el % 3 == 0 && el % 5 == 0
+      puts "FizzBuzz"
+    elsif el % 3 == 0
+      puts "Fizz"
+    elsif el % 5 == 0
+      puts "Buzz"
+    else
+      puts el
+    end
+  end
+end
+
+fizzbuzz
