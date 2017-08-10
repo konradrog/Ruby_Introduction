9d99d512fcc981b4722ebd4ba9aab094ca069296
konradrog <konrad1210@gmail.com>
2017-08-10 22:36:39 +0200

    final

--- Zadania_02_OOP/sieve_of_erastosthenes.rb
+++ Zadania_02_OOP/sieve_of_erastosthenes.rb
@@ -1,27 +1,35 @@
 class Erasto
-  attr_accessor :board
+  attr_accessor :board, :board_final
 
   def initialize(begining,ending)
     table = (begining..ending)
     @board = (table.map { |el| [el, true]}).to_h
-    @board_m = {}
+    @board_final = []
   end
 
   def doing_something
-    board_m = @board
+    board_final = @board
 
-    first_el = board_m.keys.first
-    counter = 1
-    while first_el * counter< board.keys.last
-      board_m[first_el * counter] = false
-      counter += 1
+    while board_final.has_value?(true)
+      first_el = board_final.keys.first
+      if board_final[first_el] == true && first_el != 1
+        @board_final << first_el
+      else
+        board_final.delete(first_el)
+      end
+      counter = 1
+      while first_el * counter <= board.keys.last
+        board_final[first_el * counter] = false
+        counter += 1
+      end
+      board_final.delete(first_el)
     end
-    @board_m = board_m
+    @board = board_final
   end
-
 end
 
-obliczenia = Erasto.new(2,5)
+obliczenia = Erasto.new(1,50)
 puts obliczenia.board
 obliczenia.doing_something
 puts obliczenia.board
+print obliczenia.board_final #
