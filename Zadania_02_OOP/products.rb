593dc8e56f6ce5726ddacaa9ccfb72f86511284c
konradrog <konrad1210@gmail.com>
2017-08-11 13:49:14 +0200

    done products

--- Zadania_02_OOP/products.rb
+++ Zadania_02_OOP/products.rb
@@ -1,4 +1,5 @@
 require "csv"
+require "pp"
 
 file = CSV.read("products.csv")
 #puts file[0]
@@ -24,19 +25,35 @@ def more_exp(file, price)
   return more_exp[0..3]
 end
 
-# def more_exp(file, price)
-#   cheaper = []
-#   more_exp = []
-#   file.each do |row|
-#     if row[1].to_i > price
-#       more_exp << "#{row[0]} #{row[1]}zł"
-#     elsif row[1].to_i < price
-#       cheaper << "#{row[0]} #{row[1]}zł"
-#     end
-#   end
-#
-#   return [more_exp.sort, cheaper.sort]
-# end
+def cheaper(file, price)
+  cheaper = []
+  file.each do |row|
+    if row[0] == "Product"
+    elsif row[1].to_i < price
+      cheaper << [row[0], row[1].to_f]
+    end
+  end
+  cheaper.sort_by! {|el| el[1]}
+  cheaper.map! {|el| el = "%-10s #{el[1]}zł" % el[0]}
+  return cheaper[0..4]
+end
+
+def convert(file, exchange_rates)
+  temp = []
+  file.each do |row|
+    row[1] = "%.2f" % (row[1].to_i * exchange_rates)
+    temp << row
+  end
+  temp
+end
+
+def save_to_file(file, exchange_rates, currency, new_filename)
+  table = convert(file, exchange_rates)
+  table[0][1] = "Price(#{currency})"
+  new_file = CSV.open("#{new_filename}", "wb") do |row|
+      table.each {|el| row << el}
+  end
+end
 
 def main(file,input)
   case input[0]
@@ -45,7 +62,9 @@ def main(file,input)
   when "-gt"
     puts more_exp(file, input[1].to_i)
   when "-lt"
+    puts cheaper(file, input[1].to_i)
   when "-c"
+    pp save_to_file(file, input[1].to_f, input[2], input[3])
   end
 end
 
--- Zadania_02_OOP/quick_sort.rb
+++ Zadania_02_OOP/quick_sort.rb
@@ -1,27 +1,43 @@
 input = [7, 2, 4, 7, 3, 1, 4, 6, 5, 8, 3, 9, 2, 6, 7, 6, 3]
 input2 = [74, 77, 64, 19, 43, 47, 77, 66, 47, 60, 7, 97, 71, 87, 95, 79]
 
-
-piwot = input[input.size / 2]
-temp = input
-print temp
-
-temp[temp.size / 2], temp[temp.size - 1] = temp[temp.size - 1], temp[temp.size / 2]
-
-
-puts piwot
-print temp
-puts
-
-i = 1
-j = 0
-while i < (temp.size - 2)
-  if  temp.at(i) < piwot
-    temp[j], temp[i] = temp[i], temp[j]
-    j += 1
+def q_s(input)
+  temp = input
+  temp[temp.size / 2], temp[temp.size - 1] = temp[temp.size - 1], temp[temp.size / 2]
+  piwot = temp[temp.size - 1]
+  i = 1
+  j = 0
+  while i < (temp.size - 2)
+    if  temp.at(i) < piwot
+      temp[j], temp[i] = temp[i], temp[j]
+      j += 1
+    end
+    i += 1
   end
-  i += 1
+  temp[temp.size - 1], temp[j] = temp[j], temp[temp.size - 1]
+
+  temp_left = temp[0...(temp.size / 2)]
+  temp_right = temp[(temp.size / 2)+ 1..temp.size - 1]
+  return [temp, temp_left, temp_right]
 end
 
-print temp
-puts
+
+require "pp"
+
+first_step = q_s(input)
+pp first_step
+second_step1 =  q_s(first_step[1])
+second_step2 =  q_s(first_step[2])
+
+pp second_step1
+pp second_step1
+
+third_step1 = q_s(second_step1[1])
+third_step2 = q_s(second_step1[2])
+third_step3 = q_s(second_step2[1])
+third_step4 = q_s(second_step2[2])
+
+pp third_step1
+pp third_step2
+pp third_step3
+pp third_step4
