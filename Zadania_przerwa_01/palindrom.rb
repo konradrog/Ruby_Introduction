364bbf0a59440b1803e0d52d0903fd667f7921dc
konradrog <konrad1210@gmail.com>
2017-08-16 11:11:54 +0200

    done

---
+++ Zadania_przerwa_01/palindrom.rb
@@ -0,0 +1,19 @@
+def palindrome?(input_string)
+  input_string = input_string.split(" ").join.downcase
+  if input_string == input_string.reverse
+    true
+  else
+    false
+  end
+end
+
+puts palindrome?('Kobyła ma mały bok') #=> true
+puts palindrome?('kajak')
+#=> true
+puts palindrome?('kajak i wiosło')
+
+puts palindrome?("Marzena pokazała Zakopane z ram")
+puts palindrome?("A to idiota")
+puts palindrome?("Atak kata")
+puts palindrome?("Kamil ślimak")
+puts palindrome?("Satyra rota to rarytas")
