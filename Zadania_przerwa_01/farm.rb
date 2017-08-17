0364df5b1f38c541dc1dc25fbb195e0f9406424a
konradrog <konrad1210@gmail.com>
2017-08-17 11:21:42 +0200

    done

---
+++ Zadania_przerwa_01/farm.rb
@@ -0,0 +1,47 @@
+class Animal
+  def give_sound
+  end
+end
+
+class Dog < Animal
+  def give_sound
+    puts "woof! woof!"
+  end
+end
+
+class Cat < Animal
+  def give_sound
+    puts "miau! miau!"
+  end
+end
+
+class Duck < Animal
+  def give_sound
+    puts "quack! quack!"
+  end
+end
+
+class Goose < Animal
+  def give_sound
+    puts "meee! meee!"
+  end
+end
+
+class Farm
+  attr_accessor :animals
+
+  def initialize(animals)
+    @animals = animals
+  end
+
+  def give_sounds
+    i = 0
+    while i < animals.size
+      animals[i].give_sound
+      i += 1
+    end
+  end
+end
+
+farm =  Farm.new([Dog.new, Cat.new, Duck.new, Goose.new])
+farm.give_sounds
