cad3afaca214f409ad2a0549bcf188c83db53651
konradrog <konrad1210@gmail.com>
2017-08-21 09:00:25 +0200

    to do: area of 2decker

--- Zadania_przerwa_01/Battleship/Board.rb
+++ Zadania_przerwa_01/Battleship/Board.rb
@@ -116,7 +116,7 @@ class Ship
 
   def initialize(size)
     @size = size
-    @deck = {}
+    @deck = []
     @ship_area = 0
   end
 
@@ -126,39 +126,30 @@ class Ship
   end
 
 
-  def build_ship(proper_board)
+  def build_ship(size, proper_board)
     rp = random_position
     while proper_board[rp[0]][rp[1]].ship_ship || proper_board[rp[0]][rp[1]].ship_area
       rp = random_position
     end
-    area = build_area(rp, proper_board)
-    area.each { |el| el.ship_area = true }
-    proper_board[rp[0]][rp[1]].ship_ship = true
+    case size
+    when 1
+      area = build_area_for_1(rp, proper_board)
+      area.each { |el| el.ship_area = true }
+      proper_board[rp[0]][rp[1]].ship_ship = true
+    when 2
+      deck = build_deck_for_2(rp, proper_board)
+      #area = build_area_for_2(deck, proper_board)
+      puts deck
+      puts rp
+      #deck[0].ship_ship = true
+      deck.each {|e| e.ship_ship = true}
+      proper_board[rp[0]][rp[1]].ship_ship = true
+    end
 
-    # while !proper_board[rp[0]][rp[1]].busy
-    #   area = build_area(rp, proper_board)
-    #   area.each do |el|
-    #     if !el.busy && !el.ship_area
-    #       el.busy = true
-    #       #el.ship_area = true
-    #     else
-    #       area.each { |e| e.busy = false}
-    #       rp = random_position
-    #       puts "next would be #{rp}"
-    #       next
-    #       #puts "second #{rp}"
-    #       #build_ship(proper_board)
-    #     end
-    #
-    #
-    #     # proper_board[rp[0]][rp[1]].ship_ship = true
-    #   end
-    #   proper_board[rp[0]][rp[1]].busy = true
-    # end
-    # puts "end #{rp}"
+
   end
 
-  def build_area(position, board)
+  def build_area_for_1(position, board)
     letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
     key_value = letters.index(position[0])
     #puts "key: #{key_value}"
@@ -228,6 +219,145 @@ class Ship
     end
     result
   end
+  def build_deck_for_2(position, board)
+    letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+    key_value = letters.index(position[0])
+    result = []
+
+
+    left_k = letters[key_value - 1]
+    mid_k = letters[key_value]
+    right_k = letters[key_value + 1]
+
+    if mid_k == "A"
+      top = board[mid_k][position[1] - 1]
+      right = board[right_k][position[1]]
+      bottom = board[mid_k][position[1] + 1]
+    elsif mid_k == "J"
+      top = board[mid_k][position[1] - 1]
+      bottom = board[mid_k][position[1] + 1]
+      left = board[left_k][position[1]]
+    elsif board[position[1]] == 0
+      right = board[right_k][position[1]]
+      bottom = board[mid_k][position[1] + 1]
+      left = board[left_k][position[1]]
+    elsif board[position[1]] == 9
+      top = board[mid_k][position[1] - 1]
+      right = board[right_k][position[1]]
+      left = board[left_k][position[1]]
+    else
+      top = board[mid_k][position[1] - 1]
+      right = board[right_k][position[1]]
+      bottom = board[mid_k][position[1] + 1]
+      left = board[left_k][position[1]]
+    end
+
+
+    if !top.ship_ship && !top.ship_area
+      result << top
+      puts "top"
+    elsif !right.ship_ship && !right.ship_area
+      result << right
+      puts "right"
+    elsif !bottom.ship_ship && !bottom.ship_area
+      result << bottom
+      puts "bot"
+    elsif !left.ship_ship && !left.ship_area
+      result << left
+      puts "left"
+    end
+
+    result << board[mid_k][position[1]]
+    # ustawic sortowanie, tak, żeby pierwszy był mniejszy wiersz
+  end
+  def build_area_for_2(deck, board)
+    letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+    key_value = letters.index(deck[0].position[0])
+    result = []
+
+    if deck[0].position[1] == deck[1].position[1]
+      left = letters[key_value - 1]
+      mid = letters[key_value]
+      right = letters[key_value + 1]
+    else
+      left = letters[key_value - 1]
+      mid_left = letters[key_value]
+      mid_right = letters[key_value + 1]
+      right = letters[key_value + 2]
+    end
+    #deck[0].position[1] = deck[0].position[1].to_i
+ # dla konfiguracji pionowej
+    if deck[0].position[1] == 0 #czyli pierwszy wiersz
+      if mid == "A"
+        result << mid4 = board[mid][deck[0].position[1] + 2]
+        result << right2 = board[right][deck[0].position[1]]
+        result << right3 = board[right][deck[0].position[1] + 1]
+        result << right4 = board[right][deck[0].position[1] + 2]
+      elsif mid == "J"
+        result << left2 = board[left][deck[0].position[1]]
+        result << left3 = board[left][deck[0].position[1] + 1]
+        result << left4 = board[left][deck[0].position[1] + 2]
+        result << mid4 = board[mid][deck[0].position[1] + 2]
+      else
+        result << left2 = board[left][deck[0].position[1]]
+        result << left3 = board[left][deck[0].position[1] + 1]
+        result << left4 = board[left][deck[0].position[1] + 2]
+        result << mid4 = board[mid][deck[0].position[1] + 2]
+        result << right2 = board[right][deck[0].position[1]]
+        result << right3 = board[right][deck[0].position[1] + 1]
+        result << right4 = board[right][deck[0].position[1] + 2]
+      end
+    elsif deck[0].position[1] == 8 #ostatni wiersz
+      if mid == "A"
+        result << mid1 = board[mid][deck[0].position[1] - 1]
+        result << right1 = board[right][deck[0].position[1] - 1]
+        result << right2 = board[right][deck[0].position[1]]
+        result << right3 = board[right][deck[0].position[1] + 1]
+      elsif mid == "J"
+        result << left1 = board[left][deck[0].position[1] - 1]
+        result << left2 = board[left][deck[0].position[1]]
+        result << left3 = board[left][deck[0].position[1] + 1]
+        result << mid1 = board[mid][deck[0].position[1] - 1]
+      else
+        result << left1 = board[left][deck[0].position[1] - 1]
+        result << left2 = board[left][deck[0].position[1]]
+        result << left3 = board[left][deck[0].position[1] + 1]
+        result << mid1 = board[mid][deck[0].position[1] - 1]
+        result << right1 = board[right][deck[0].position[1] - 1]
+        result << right2 = board[right][deck[0].position[1]]
+        result << right3 = board[right][deck[0].position[1] + 1]
+      end
+    elsif mid == "A"
+      result << mid1 = board[mid][deck[0].position[1] - 1]
+      result << mid4 = board[mid][deck[0].position[1] + 2]
+      result << right1 = board[right][deck[0].position[1] - 1]
+      result << right2 = board[right][deck[0].position[1]]
+      result << right3 = board[right][deck[0].position[1] + 1]
+      result << right4 = board[right][deck[0].position[1] + 2]
+    elsif mid == "J"
+      result << left1 = board[left][deck[0].position[1] - 1]
+      result << left2 = board[left][deck[0].position[1]]
+      result << left3 = board[left][deck[0].position[1] + 1]
+      result << left4 = board[left][deck[0].position[1] + 2]
+      result << mid1 = board[mid][deck[0].position[1] - 1]
+      result << mid4 = board[mid][deck[0].position[1] + 2]
+    else #to wyjściowo
+      result << left1 = board[left][deck[0].position[1] - 1]
+      result << left2 = board[left][deck[0].position[1]]
+      result << left3 = board[left][deck[0].position[1] + 1]
+      result << left4 = board[left][deck[0].position[1] + 2]
+      result << mid1 = board[mid][deck[0].position[1] - 1]
+      result << mid4 = board[mid][deck[0].position[1] + 2]
+      result << right1 = board[right][deck[0].position[1] - 1]
+      result << right2 = board[right][deck[0].position[1]]
+      result << right3 = board[right][deck[0].position[1] + 1]
+      result << right4 = board[right][deck[0].position[1] + 2]
+    end
+
+
+  end
+
+
 end
 
 
@@ -239,11 +369,18 @@ ship = Ship.new(1)
 ship2 = Ship.new(1)
 ship3 = Ship.new(1)
 ship4 = Ship.new(1)
-ship.build_ship(board.proper_board)
+ship5 = Ship.new(2)
+ship6 = Ship.new(2)
+ship7 = Ship.new(2)
+
+ship.build_ship(1, board.proper_board)
 #board.draw_board
-ship2.build_ship(board.proper_board)
+ship2.build_ship(1, board.proper_board)
 #board.draw_board
-ship3.build_ship(board.proper_board)
+ship3.build_ship(1, board.proper_board)
 #board.draw_board
-ship4.build_ship(board.proper_board)
+ship4.build_ship(1, board.proper_board)
+ship5.build_ship(2, board.proper_board)
+ship6.build_ship(2, board.proper_board)
+ship7.build_ship(2, board.proper_board)
 board.draw_board
