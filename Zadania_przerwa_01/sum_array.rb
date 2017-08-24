f4f411d522d7fd0beffbc7ae63f38ae49a238288
konradrog <konrad1210@gmail.com>
2017-08-24 18:18:21 +0200

    after feedback

--- Zadania_przerwa_01/sum_array.rb
+++ Zadania_przerwa_01/sum_array.rb
@@ -1,10 +1,9 @@
 def sum_array(array)
-  array.delete(array.min)
-  array.delete(array.max)
-  puts array.inject(0) { |acc, el| acc + el}
+  result =  array.inject(0) { |acc, el| acc + el}
+  result -= array.min + array.max
 end
 
-sum_array([1, 2, 3, 4])
-sum_array([12, 4, 0, -8])
-sum_array([12, 4])
-sum_array([120, 7, 145, -10, -15])
+puts sum_array([1, 2, 3, 4])
+puts sum_array([12, 4, 0, -8])
+puts sum_array([12, 4])
+puts sum_array([120, 7, 145, -10, -15])
