a1729dd615ca6cda7b209aeef59d219c7c24f3dd
konradrog <konrad1210@gmail.com>
2017-08-24 21:06:35 +0200

    after feedback

--- Zadania_przerwa_01/backspace.rb
+++ Zadania_przerwa_01/backspace.rb
@@ -3,17 +3,19 @@ def backspace(string)
     return string
   end
   counter = 0
-  size = string.length
-  result = ""
   while string.include? "#"
-    string[string.index("#") - 1] = ""
-    string[string.index("#")] = ""
+    if string.index("#") - 1 >= 0
+      string[string.index("#") - 1] = ""
+      string[string.index("#")] = ""
+    else
+      string[string.index("#")] = ""
+    end
   end
-  puts string
+  string
 end
 
-backspace("a#bc#d") #{}"bd"
-backspace("abc#d##c") # "ac"
-backspace("abc##d######") #jest równy ""
-backspace("######") #jest równy ""
-backspace("") #jest równy (")
+puts backspace("a#bc#d") #{}"bd"
+puts backspace("abc#d##c") # "ac"
+puts backspace("abc##d######") #jest równy ""
+puts backspace("######") #jest równy ""
+puts backspace("") #jest równy (")
