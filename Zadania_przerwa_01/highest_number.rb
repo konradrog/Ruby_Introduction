0ef802f075c99b7c33cb998d7f4241ccda0930b4
konradrog <konrad1210@gmail.com>
2017-08-24 18:33:24 +0200

    after feedback

--- Zadania_przerwa_01/highest_number.rb
+++ Zadania_przerwa_01/highest_number.rb
@@ -1,11 +1,15 @@
 def highest_number(number)
-  number = number.to_s
   temp = []
+  number = number.to_s
   number.each_char {|char| temp << char}
-  print temp.sort.reverse.join
-  puts
+  if number[0] == "-"
+    result = temp.sort.join.to_i
+  else
+    result = temp.sort.reverse.join.to_i
+  end
 end
 
-highest_number(132)
-highest_number(1464)
-highest_number(165423) #=> 654321
+puts highest_number(132)
+puts highest_number(1464)
+puts highest_number(165423) #=> 654321
+puts highest_number(-165423)
