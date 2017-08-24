c717bf53a90895b3977197d41ddf8a160fd1b4f6
konradrog <konrad1210@gmail.com>
2017-08-24 22:30:21 +0200

    after feedback

--- Zadania_przerwa_01/consecutices_sum.rb
+++ Zadania_przerwa_01/consecutices_sum.rb
@@ -1,27 +1,26 @@
 def consecutives_sum(array)
   result = []
-  counter = 0
   temp = 0
-  while counter < array.size
-    if array[counter] == array[counter + 1] && array[counter] != array[array.size - 1]
-      temp += array[counter]
+  array.each_with_index do |el, i|
+    if array[i] == array[i + 1]
+      puts "*"
+      temp += array[i]
     else
       if temp != 0
-        temp += array[counter]
+        temp += array[i]
         result << temp
         temp = 0
       else
-        result << array[counter]
+        result << array[i]
       end
     end
-    counter += 1
   end
-  print result
-  puts
+  result
 end
 
-consecutives_sum([1, 4, 4, 4, 0, 4, 3, 3, 1]) #=> [1, 12, 0, 4, 6, 1]
-consecutives_sum([1, 1, 7, 7, 3])
-#=> [2, 14, 3]
-consecutives_sum([-5, -5, 7, 7, 12, 0])
+print consecutives_sum([1, 4, 4, 4, 0, 4, 3, 3, 1, 1]) #=> [1, 12, 0, 4, 6, 1]
+puts
+print consecutives_sum([1, 1, 7, 7, 3])
+puts#=> [2, 14, 3]
+print consecutives_sum([-5, -5, 7, 7, 12, 0])
 #=> [-10, 14, 12, 0]
