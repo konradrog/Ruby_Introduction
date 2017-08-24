aada4a6582c9986bd72581b6d6c451f3a3b48fd8
konradrog <konrad1210@gmail.com>
2017-08-24 20:34:36 +0200

    after feedback

--- Zadania_przerwa_01/leap_year.rb
+++ Zadania_przerwa_01/leap_year.rb
@@ -1,9 +1,5 @@
 def leap_year?(year)
-  if (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)
-    true
-  else
-    false
-  end
+  (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)
 end
 
 
