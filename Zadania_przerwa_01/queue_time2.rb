de142f615a3149039b43cdb4261d5dde1e313890
konradrog <konrad1210@gmail.com>
2017-08-28 21:42:43 +0200

    After 2nd feedback

--- Zadania_przerwa_01/queue_time2.rb
+++ Zadania_przerwa_01/queue_time2.rb
@@ -11,36 +11,28 @@ class Checkout
 end
 
 def queue_time(client_times, number_of_checkouts)
-  clients = client_times
+  clients = client_times.dup
   result = []
   checkouts = []
   number_of_checkouts.times do
     checkouts << Checkout.new
   end
 
-  if number_of_checkouts == 1;
-    result = clients.sum
-  else
+  while clients.size > 0
+    temp = []
     checkouts.each do |val|
-      val.add_client(clients.first)
-      clients.shift
+      temp << val.service_time
     end
-    while clients.size > 0
-      temp = []
-      checkouts.each do |val|
-        temp << val.service_time
-      end
-      min_index = temp.find_index(temp.min)
-      checkouts[min_index].add_client(clients.first)
-      clients.shift
-    end
-    checkouts.each { |e| result << e.service_time }
-    result = result.max
+    min_index = temp.find_index(temp.min)
+    checkouts[min_index].add_client(clients.first)
+    clients.shift
   end
+  checkouts.each { |e| result << e.service_time }
+  result = result.max
 end
 
 puts queue_time([5, 3, 4], 1) #=> 12
-puts queue_time(table, 2) #=> 10
+puts queue_time([10, 2, 3, 3], 2) #=> 10
 puts queue_time([2, 3, 10], 2) #=> 12
 puts queue_time([5, 6, 7, 2, 10, 11], 3) #=> 18
 puts queue_time([2,2,2],3)
