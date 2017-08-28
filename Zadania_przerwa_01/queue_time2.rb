b2c3d13e738457186d938123fdade374b1cb9851
konradrog <konrad1210@gmail.com>
2017-08-28 20:37:09 +0200

    puts removed

--- Zadania_przerwa_01/queue_time2.rb
+++ Zadania_przerwa_01/queue_time2.rb
@@ -25,36 +25,24 @@ def queue_time(client_times, number_of_checkouts)
       val.add_client(clients.first)
       clients.shift
     end
-    puts "clients after first tour #{clients}"
     while clients.size > 0
       temp = []
       checkouts.each do |val|
         temp << val.service_time
       end
-      puts "temp values #{temp}"
       min_index = temp.find_index(temp.min)
-      puts "first client #{clients.first}"
       checkouts[min_index].add_client(clients.first)
-
       clients.shift
     end
-
-
-    puts "min: #{temp.find_index(temp.min)}"
-
     checkouts.each { |e| result << e.service_time }
     result = result.max
   end
-  puts "rest of clients #{clients}"
-  puts "result: #{result}"
-  puts "****************************"
-  result
 end
 
-
-
-
 puts queue_time([5, 3, 4], 1) #=> 12
-puts queue_time([10, 2, 3, 3], 2) #=> 10
+puts queue_time(table, 2) #=> 10
 puts queue_time([2, 3, 10], 2) #=> 12
 puts queue_time([5, 6, 7, 2, 10, 11], 3) #=> 18
+puts queue_time([2,2,2],3)
+puts queue_time([4, 5, 3, 15], 3)
+puts queue_time([4, 5, 3, 15, 14, 13], 3)
